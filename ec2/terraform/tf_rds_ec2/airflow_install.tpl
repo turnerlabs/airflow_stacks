25e77040ab095f521f21758f76f4468817be9d36
Michael Smith <smithatlanta@gmail.com>
2018-05-01 15:16:28 -0400

    Using existing python3 ami, create just an airflow webserver and an rds(myslq) database

---
+++ ec2/terraform/tf_rds_ec2/airflow_install.tpl
@@ -0,0 +1,32 @@
+#!/bin/bash -xe
+
+mysql --host=${rds_url} --user=${db_master_username} --password=${db_master_password} -e "CREATE DATABASE IF NOT EXISTS ${airflow_dbname} /*\!40100 DEFAULT CHARACTER SET ${airflow_db_charset} */;"
+mysql --host=${rds_url} --user=${db_master_username} --password=${db_master_password} -e "CREATE USER IF NOT EXISTS '${db_airflow_username}'@'%' IDENTIFIED BY '${db_airflow_password}';"
+mysql --host=${rds_url} --user=${db_master_username} --password=${db_master_password} -e "GRANT ALL PRIVILEGES ON ${airflow_dbname}.* TO '${db_airflow_username}'@'%';"
+mysql --host=${rds_url} --user=${db_master_username} --password=${db_master_password} -e "FLUSH PRIVILEGES;"
+
+export AIRFLOW_HOME=/home/ubuntu/airflow
+
+cd /home/ubuntu/airflow
+
+
+sed -i -e "s/expose_config = False/expose_config = True/g" airflow.cfg
+sed -i -e "s/remote_log_conn_id =/remote_log_conn_id = s3_logging_conn/g" airflow.cfg
+sed -i -e "s/load_examples = True/load_examples = False/g" airflow.cfg
+sed -i -e "s/authenticate = False/authenticate = True/g" airflow.cfg
+sed -i -e "s/sql_alchemy_conn = sqlite:\/\/\/\/home\/ubuntu\/airflow\/airflow.db/sql_alchemy_conn = mysql:\/\/${db_airflow_username}:${db_airflow_password}@${rds_url}\/${airflow_dbname}/g" airflow.cfg
+sed -i -e "/auth_backend = airflow.api.auth.backend.default/d" airflow.cfg
+sed -i -e "/\[webserver\]/a\\
+auth_backend = airflow.contrib.auth.backends.password_auth" airflow.cfg
+sed -i -e "/\[core\]/a\\
+remote_base_log_folder = s3://${s3_log_bucket_name}" airflow.cfg
+
+/home/ubuntu/venv/bin/airflow initdb
+
+systemctl enable airflow-webserver
+
+systemctl daemon-reload
+
+systemctl start airflow-webserver
+
+/home/ubuntu/venv/bin/python /home/ubuntu/adduser.py -u ${airflow_username} -e ${airflow_emailaddress} -p ${airflow_password}
---
+++ ec2/terraform/tf_rds_ec2/variables.tf
@@ -0,0 +1,91 @@
+variable "region" {
+  description = "Region"
+  default     = "us-east-1"
+}
+
+variable "profile" {
+  description = "Profile from credentials"
+  default     = "default"
+}
+
+variable "tag_application" {}
+variable "tag_contact_email" {}
+variable "tag_customer" {}
+variable "tag_team" {}
+variable "tag_environment" {}
+
+variable "vpc_id" {
+  description = "VPC to launch into"
+}
+
+variable "subnet_id1" {
+  description = "Subnet to launch RDS into. Hopefully in different AZ"
+}
+
+variable "subnet_id2" {
+  description = "Subnet to launch RDS into. Hopefully in different AZ"
+}
+
+variable "db_instance_class" {
+  description = "Database instance size"
+  default     = "db.t2.medium"
+}
+
+variable "db_identifier" {
+  description = "Database identifier"
+  default     = "airflow_rds"
+}
+
+variable "airflow_ami" {
+  description = "Airflow AMI created by packer"
+}
+
+variable "airflow_instance_class" {
+  description = "Airflow instance size"
+  default     = "t2.large"
+}
+
+variable "airflow_keypair_name" {
+  description = "AWS keypair to use on the airflow ec2 instance"
+}
+
+variable "db_master_username" {
+  description = "MySQL master username"
+  default     = "admin"
+}
+
+variable "db_master_password" {}
+
+variable "db_airflow_username" {
+  description = "MySQL Airflow username"
+  default     = "airflow"
+}
+
+variable "db_airflow_password" {}
+
+variable "airflow_dbname" {
+  description = "MYSQL airflow database name"
+  default     = "airflow"
+}
+
+variable "airflow_db_charset" {
+  description = "MYSQL airflow database character set"
+  default     = "latin1"
+}
+
+variable "airflow_username" {
+  description = "Airflow username for website access"
+  default     = "airflow"
+}
+
+variable "airflow_emailaddress" {
+  description = "Airflow emailaddress for website access"
+}
+
+variable "airflow_password" {
+  description = "Airflow password for website access"
+}
+
+variable "s3_log_bucket_name" {
+  description = "Airflow bucket to eventually log to"
+}
