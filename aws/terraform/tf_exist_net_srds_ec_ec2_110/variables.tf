25f8a42c5064e3c139b42e4f31146e20ea7066fa
Michael Smith <smithatlanta@gmail.com>
2018-10-04 14:55:02 -0400

    added variables needed for naming

--- aws/terraform/tf_exist_net_srds_ec_ec2_110/variables.tf
+++ aws/terraform/tf_exist_net_srds_ec_ec2_110/variables.tf
@@ -178,3 +178,11 @@ variable "ingress_ip_description"  {
 variable "aws_account_number" {
   description = "AWS account number"  
 }
+
+variable "domain" {
+  description = "Domain for Route53"  
+}
+
+variable "subdomain" {
+  description = "Sub Domain for cert"
+}
