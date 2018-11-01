4d3a98bf62443a438f554df0669f019080cb3fa8
Michael Smith <smithatlanta@gmail.com>
2018-11-01 17:17:59 -0400

    added lifecycle policies to s3 buckets. (#58)

--- aws/terraform/tf_new_net_rds_ec_ec2_110/main.tf
+++ aws/terraform/tf_new_net_rds_ec_ec2_110/main.tf
@@ -355,6 +355,25 @@ resource "aws_s3_bucket" "s3_airflow_log_bucket" {
   bucket        = "${var.prefix}${var.s3_airflow_log_bucket_name}"
   force_destroy = "true"
 
+  lifecycle_rule {
+    id      = "airflow_task_log"
+    enabled = true
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 180
+    }
+  }
+
   tags {
     application     = "${var.tag_application}"
     contact-email   = "${var.tag_contact_email}"
@@ -368,6 +387,26 @@ resource "aws_s3_bucket" "s3_airflow_log_bucket" {
 resource "aws_s3_bucket" "s3_airflow_bucket" {
   bucket        = "${var.prefix}${var.s3_airflow_bucket_name}"
   force_destroy = "true"
+  
+  lifecycle_rule {
+    id      = "airflow_server_log"
+    enabled = true
+    prefix  = "logs/"
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 180
+    }
+  }
 
   tags {
     application     = "${var.tag_application}"
@@ -388,6 +427,24 @@ resource "aws_s3_bucket" "s3_airflow_access_log_bucket" {
     customer        = "${var.tag_customer}"
     team            = "${var.tag_team}"
     environment     = "${var.tag_environment}"
+  }
+  lifecycle_rule {
+    id      = "airflow_access_logs"
+    enabled = true
+    
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 365
+    }
   }
 
 policy = <<EOF
