cba599b66edd553c11d1e8081a205bc66fedc29d
Michael Smith <smithatlanta@gmail.com>
2018-11-15 15:31:11 -0500

    added shell script to install aiflow variables

--- aws/packer/airflow_websched_python3/scripts/pip_mod_install.sh
+++ aws/packer/airflow_websched_python3/scripts/pip_mod_install.sh
@@ -10,13 +10,7 @@ echo "---------- STARTED at $DATESTART ----------" >> $PIP_LOG_FILE
 # verify file exists
 if [ -e $PIP_MODULES_FILE ]
 then
-    # if file exists
-    # loop through file 
-    for i in $(cat ${PIP_MODULES_FILE}); do
-        # pip install modules
-        # pipe all output from the attempted install into a file in AIRFLOW_HOME/logs call <instance_name>_module_install.log
-        /home/ubuntu/venv/bin/pip install -qqq --no-color --upgrade --log $PIP_LOG_FILE $i
-    done
+    /home/ubuntu/venv/bin/pip install -qqq --no-color --upgrade --requirement $PIP_MODULES_FILE --log $PIP_LOG_FILE
 else
     echo "No file found at $PIP_MODULES_FILE to install." >> $PIP_LOG_FILE
 fi
