191fbeb0fffbd089060bacc62949a5f29fd84b84
Michael Smith <smithatlanta@gmail.com>
2018-12-11 13:14:02 -0500

    added unixodbc to packages

--- aws/packer/airflow_worker_python3/provision.sh
+++ aws/packer/airflow_worker_python3/provision.sh
@@ -8,7 +8,7 @@ echo "------------------- apt update complete -------------------"
 sudo apt-get -y install unattended-upgrades
 echo "------------------- apt upgrade complete -------------------"
 
-sudo apt-get -y install chrony jq virtualenv python3-pip libmysqlclient-dev python3-dev python3 libkrb5-dev libsasl2-dev mysql-client-core-5.7 python3-gdbm redis-tools openjdk-8-jre
+sudo apt-get -y install unixodbc chrony jq virtualenv python3-pip libmysqlclient-dev python3-dev python3 libkrb5-dev libsasl2-dev mysql-client-core-5.7 python3-gdbm redis-tools openjdk-8-jre
 echo "------------------- airflow aptitude dependencies complete -------------------"
 
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
